96566fd35dafeab81f9b706702b30527a1b32cfd
HarnoorbirSingh01 <126174543+HarnoorbirSingh01@users.noreply.github.com>
2023-09-03 21:00:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let o = document.getElementById("one");
+let t = document.getElementById("two");
+let th = document.getElementById("three");
+
+
+t.addEventListener('click', function(){
+    location.href = "two.html"
+})
+o.addEventListener('click', function(){
+    location.href = "one.html"
+})
+th.addEventListener('click', function(){
+    location.href = "three.html"
+})
+
